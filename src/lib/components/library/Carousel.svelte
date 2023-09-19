5d927635f9e0cd0b5b38451f243c5f16d16cc3f0
PintoGideon <gideonpinto123@gmail.com>
2023-09-19 12:43:45 -0400

    Semi average knowledge of typescript

--- src/lib/components/library/Carousel.svelte
+++ src/lib/components/library/Carousel.svelte
@@ -1,121 +1,107 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import type { PreviewPayload } from "$lib/types/Library";
-  import {
-    DcmDisplay,
-    ImageDisplay,
-    IFrameDisplay,
-  } from "$components/displays";
-  import { Button } from "$lib/components/ui/button";
-  import {
-    fetchFile,
-    getFileExtension,
-    fileViewerMap,
-  } from "$lib/utilities/library";
-  import type { File } from "$lib/types/Library";
+	import { onMount } from 'svelte';
+	import type { PreviewPayload } from '$lib/types/Library';
+	import { DcmDisplay, ImageDisplay, IFrameDisplay} from '$components/displays';
+	import { Button } from '$lib/components/ui/button';
+	import { fetchFile, getFileExtension, fileViewerMap } from '$lib/utilities/library';
 
-  export let token: string;
-  export let previewPayload: PreviewPayload;
-  export let files: File[];
-  export let dispatchFileActions: (
-    action: string,
-    file: any,
-    currentIndex?: number
-  ) => void;
+	import type { FileType } from '$lib/types/Data';
 
-  const components: any = {
-    DcmDisplay,
-    ImageDisplay,
-    IFrameDisplay,
-  };
+	export let token: string;
+	export let previewPayload: PreviewPayload;
+	export let files: FileType[];
+	export let dispatchFileActions: (action: string, file: any, currentIndex?: number) => void;
 
-  let ViewerType: any;
-  let fileBlob: Blob;
-  let fileExtension: string;
+	type Components = {
+		DcmDisplay: typeof DcmDisplay;
+		ImageDisplay: typeof ImageDisplay;
+		IFrameDisplay: typeof IFrameDisplay;
+	};
 
-  async function fetchData(fname: string, token: string, type: string) {
-    const fileData = await fetchFile(fname, token, type);
-    let fileBlob: Blob;
-    let fileExtension: string;
+	const components: Components = {
+		DcmDisplay,
+		ImageDisplay,
+		IFrameDisplay
+	};
 
-    if (type === "file") {
-      fileBlob = await fileData.getFileBlob();
-      fileExtension = getFileExtension(fname);
-    } else {
-      const middle = Math.floor(fileData.length / 2);
-      const chosenFile = fileData[middle];
-      fileBlob = await chosenFile.getFileBlob();
-      fileExtension = getFileExtension(chosenFile.data.fname);
-    }
+	let ViewerType: keyof Components | undefined;
+	let fileBlob: Blob;
+	let fileExtension: string;
 
-    return {
-      blob: fileBlob,
-      fileExtension,
-    };
-  }
+	async function fetchData(fname: string, token: string, type: string) {
+		const fileData = await fetchFile(fname, token, type);
+		let fileBlob: Blob;
+		let fileExtension: string;
 
-  async function setViewer(fname: string, type: string) {
-    const data = await fetchData(fname, token, type);
-    ViewerType = fileViewerMap[data.fileExtension];
-    fileBlob = data.blob;
-    fileExtension = data.fileExtension;
-  }
+		if (type === 'file') {
+			fileBlob = await fileData.getFileBlob();
+			fileExtension = getFileExtension(fname);
+		} else {
+			const middle = Math.floor(fileData.length / 2);
+			const chosenFile = fileData[middle];
+			fileBlob = await chosenFile.getFileBlob();
+			fileExtension = getFileExtension(chosenFile.data.fname);
+		}
 
-  onMount(async () => {
-    const { payload } = previewPayload;
+		return {
+			blob: fileBlob,
+			fileExtension
+		};
+	}
 
-    if (previewPayload.type === "file") {
-      setViewer(payload.fname, "file");
-    }
+	async function setViewer(fname: string, type: string) {
+		const data = await fetchData(fname, token, type);
+		ViewerType = fileViewerMap[data.fileExtension];
+		fileBlob = data.blob;
+		fileExtension = data.fileExtension;
+	}
 
-    if (previewPayload.type === "folder") {
-      setViewer(`${payload.path}/${payload.name}`, "folder");
-    }
-  });
+	onMount(async () => {
+		const { payload } = previewPayload;
 
-  function loadNext() {
-    const { currentIndex } = previewPayload;
+		if (previewPayload.type === 'file') {
+			setViewer(payload.fname, 'file');
+		}
 
-    if (currentIndex !== undefined) {
-      const file = files[currentIndex + 1];
-      dispatchFileActions("Preview", file, currentIndex + 1);
-    }
-  }
+		if (previewPayload.type === 'folder') {
+			setViewer(`${payload.path}/${payload.name}`, 'folder');
+		}
+	});
 
-  function loadPrevious() {
-    const { currentIndex } = previewPayload;
-    if (currentIndex !== undefined) {
-      const file = files[currentIndex - 1];
-      dispatchFileActions("Preview", file, currentIndex - 1);
-    }
-  }
+	function loadNext() {
+		const { currentIndex } = previewPayload;
 
-  $: if (previewPayload.type === "file") {
-    setViewer(previewPayload.payload.fname, "file");
-  }
-  
+		if (currentIndex !== undefined) {
+			const file = files[currentIndex + 1];
+			dispatchFileActions('Preview', file, currentIndex + 1);
+		}
+	}
+
+	function loadPrevious() {
+		const { currentIndex } = previewPayload;
+		if (currentIndex !== undefined) {
+			const file = files[currentIndex - 1];
+			dispatchFileActions('Preview', file, currentIndex - 1);
+		}
+	}
+
+	$: if (previewPayload.type === 'file') {
+		setViewer(previewPayload.payload.fname, 'file');
+	}
 </script>
 
 {#if !ViewerType}
-  <div>Loading a viewer for this file type</div>
+	<div>Loading a viewer for this file type</div>
 {:else}
-  {#if previewPayload.type === "file"}
-    <Button
-      on:click={loadPrevious}
-      class="top-4 left-4 absolute z-40"
-      variant="ghost">Load Previous</Button
-    >
+	{#if previewPayload.type === 'file'}
+		<Button on:click={loadPrevious} class="top-4 left-4 absolute z-40" variant="ghost"
+			>Load Previous</Button
+		>
 
-    <Button
-      on:click={loadNext}
-      class="top-4 right-10 absolute z-40"
-      variant="ghost">Load Next</Button
-    >
-  {/if}
+		<Button on:click={loadNext} class="top-4 right-10 absolute z-40" variant="ghost"
+			>Load Next</Button
+		>
+	{/if}
 
-  <svelte:component
-    this={components[ViewerType]}
-    fileType={fileExtension}
-    blob={fileBlob}
-  />
+	<svelte:component this={components[ViewerType]} fileType={fileExtension} blob={fileBlob} />
 {/if}
